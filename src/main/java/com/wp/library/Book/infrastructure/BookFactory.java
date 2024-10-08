79791b419676b62e8dff0f09d83344902e6fe4e7
michalek990 <93988101+michalek990@users.noreply.github.com>
2024-10-08 22:11:41 +0200

    make BookFactory package-private

--- src/main/java/com/wp/library/Book/infrastructure/BookFactory.java
+++ src/main/java/com/wp/library/Book/infrastructure/BookFactory.java
@@ -5,13 +5,13 @@ import com.wp.library.Book.domain.book.PrintedBook;
 import org.springframework.stereotype.Component;
 
 @Component
-public class BookFactory {
+class BookFactory {
 
-    public static Ebook createEbook(String title, String description, Integer rate, String isbn, String fileFormat) {
+    static Ebook createEbook(String title, String description, Integer rate, String isbn, String fileFormat) {
         return new Ebook(title, description, rate, isbn, fileFormat);
     }
 
-    public static PrintedBook createPrintedBook(String title, String description, Integer rate, String isbn, Integer numberOfPages) {
+    static PrintedBook createPrintedBook(String title, String description, Integer rate, String isbn, Integer numberOfPages) {
         return new PrintedBook(title, description, rate, isbn, numberOfPages);
     }
 }
